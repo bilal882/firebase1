8bda79189d55557f35f13bf2b63352f06c0b0f05
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-06-29 12:15:43 +0500

    Login form complete
    is ma hum login kr saktay hain or logout kr saktay hain or hum ko data firebase mai milay ga or hum os data ko delete or disable kr saktay hain
    jo user aik bar login ho jay wo dobara login nahi ho sakt

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -5,86 +5,60 @@ import { auth } from '../Firebase/Firebase';
 export default function Login() {
     const initialData = { email: "", password: "" };
     const [state, setState] = useState(initialData);
-    const [users, setUsers] = useState("");
-
+    const [users, setUsers] = useState({});
 
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
             if (user) {
+                setUsers(user)
                 console.log(user);
-                setUsers("You Are Logged In Thanks For Log In", user.email)
-                console.log("You are logged In");
             } else {
-                console.log("You Are Not Log In Please Register To Login");
-                return (
-                    <div className="row text-center">
-                        <div className="col">
-                            <div className="card">
-                                <form onSubmit={submitHandler}>
-                                    <h2 className='text-center my-2'>Login Page</h2>
-                                    <div className="card-body">
-                                        <input type="email" onChange={handleChange} name="email" placeholder="Email" className='form-control my-3' />
-                                        <input type="password" name="password" placeholder="Password" onChange={handleChange} className='form-control my-3' />
-                                        <button className="btn btn-success w-100">Login</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                )
             }
         });
 
     }, [])
-
     const handleChange = (e) => {
         setState({ ...state, [e.target.name]: e.target.value })
     }
     const submitHandler = (e) => {
         e.preventDefault();
-        console.log(state);
-        const { email, password } = state;
-        // const auth = getAuth();
-        createUserWithEmailAndPassword(auth, email, password)
+        const { email, password, name } = state;
+        createUserWithEmailAndPassword(auth, email, password, name)
             .then((userCredential) => {
                 const user = userCredential.user;
-                console.log(user);
-                setUsers(user.email)
             })
             .catch((error) => {
                 console.error(error);
-            });
+            })
     }
+
     const handleLogout = () => {
         signOut(auth).then(() => {
-            console.log("You are Logged Out.");
+            setUsers({});
         }).catch((error) => {
             console.error(error);
         });
-
     }
-    // const [state, setState] = useState(initialData);
     return (
         <div className='login'>
             <div className="container">
                 {
-                    users ?
+                    users.email ?
                         <div className="row">
                             <div className="col">
-                                <h2 className='text-center'> {users} </h2><br /><br />
+                                <h2 className='text-center'>You are Logged In and Your Email is : {users.email} </h2><br /><br />
                                 <button onClick={handleLogout} className="btn btn-danger text-center">Logout</button>
                             </div>
                         </div>
                         :
-
                         <div className="row text-center">
-                            <div className="col">
+                            <div className="col-12 col-md-6 offset-md-3 col-lg-8 md-2">
                                 <div className="card">
                                     <form onSubmit={submitHandler}>
                                         <h2 className='text-center my-2'>Login Page</h2>
                                         <div className="card-body">
-                                            <input type="email" onChange={handleChange} name="email" placeholder="Email" className='form-control my-3' />
-                                            <input type="password" name="password" placeholder="Password" onChange={handleChange} className='form-control my-3' />
+                                            <input type="email" onChange={handleChange} name="email" placeholder="Email" className='form-control my-3' required />
+                                            <input type="password" name="password" placeholder="Password" onChange={handleChange} className='form-control my-3' required />
                                             <button className="btn btn-success w-100">Login</button>
                                         </div>
                                     </form>
